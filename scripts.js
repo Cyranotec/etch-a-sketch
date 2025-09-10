3c85b03270e485797f1130b26b7052981dca1100
Cyranotec <cyranotec@gmail.com>
2025-09-10 14:57:11 -0600

    -Generar la cuadrícula con el número de n columnas -Utilizar una variable en el css de los div dentro de #container, para cada div se indica opacidad .1 y se añade el manejador de eventos follow, se utiliza un cálculo con una variable css para que el ancho de cada div sea 100%/n -La función follow (manejador de mouseenter) aplica un color rgb aleatorio, toma la opacidad del elemento y la incrementa en .1 hasta 1

---
+++ scripts.js
@@ -0,0 +1,42 @@
+const container = document.querySelector('#container');
+const log = document.querySelector('#log');
+
+function generateGrid(x) {
+    container.innerHTML = '';
+    total = (x * x) + 1;
+    for (let i = 1; i < total; i++) {
+        const div = document.createElement('div');
+        div.textContent = i;
+        div.style.setProperty('opacity', '.1');
+        div.addEventListener('mouseenter', follow)
+
+        container.appendChild(div);
+    }
+    document.documentElement.style.setProperty('--columns', x)
+}
+
+function askForColumns() {
+    const columnas = prompt('Indica el número de columnas (<=64)');
+    if (columnas <= 64) {
+        generateGrid(columnas);
+    }
+    else {
+        alert('El número de columnas debe ser igual o menor a 64');
+    }
+}
+
+function follow(e) {
+    // poner color aleatorio al MediaElementAudioSourceNode
+    const color = `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)})`;
+    e.target.style.setProperty('background-color', color);
+    // aumentar la opacidad hasta llegar a q
+    const opacidad = parseFloat(window.getComputedStyle(e.target).opacity);
+    const nuevaOpacidad = Math.min(opacidad + .1, 1);
+    e.target.style.setProperty('opacity', nuevaOpacidad);
+
+    console.log(`${e.target.textContent} : ${nuevaOpacidad}`)
+
+}
+
+
+document.querySelector('#genNew').addEventListener('click', askForColumns);
